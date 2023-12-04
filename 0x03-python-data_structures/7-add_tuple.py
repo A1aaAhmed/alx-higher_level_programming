2745b0a5fb1ac8450beb2610f52b79e766316aae
A1aaAhmed <alaaahmad20000@gmail.com>
2023-12-04 15:03:41 -0800

    tasks

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
-"""
-    /**
-      * update_dictionary - eplaces or adds key/value in a dictionary
-      * @a_dictionary: dictionary
-      * @key: key
-      * @value: value
-      *
-      * Return: dictionary
-      */
-"""
+def add_tuple(tuple_a=(), tuple_b=()):
+    ta = resolve(tuple_a)
+    tb = resolve(tuple_b)
+    return (ta[0] + tb[0], ta[1] + tb[1])
 
 
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update([(key, value)])
-    return a_dictionary
+def resolve(t=()):
+    tlen = len(t)
+    if tlen == 0:
+        tnew = 0, 0
+    elif tlen == 1:
+        tnew = t[0], 0
+    else:
+        tnew = t[0], t[1]
+    return tnew
