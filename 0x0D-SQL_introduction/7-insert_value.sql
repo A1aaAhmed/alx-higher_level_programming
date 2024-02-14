a9d5688dfa1838791b7b01a5b94aca8bef11b96e
A1aaAhmed <alaaahmad20000@gmail.com>
2024-02-14 10:36:14 -0800

    first three tasks

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,6 +1,4 @@
--- Write a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
--- id = 89
--- name = Best School
--- The database name will be passed as an argument of the mysql command
-INSERT INTO first_table (id, name) VALUES {89, 'Best School'};
-
+-- Inserts a new row in table 'first_table' of 'hbtn_0c_0' database
+-- New row: id = 89, name = Best School
+INSERT INTO first_table(id, name)
+VALUES(89, 'Best School');
